3fe686b76f1e7e72e6428d46c999dc4f6ac89757
PlatonFPS <PlatonFPS@yandex.ru>
2023-10-08 18:37:13 +0300

    Upgraded 33b(WIP)
    Fixed runtime errors, now i get time limit on test 26(probably need not to check every node on graph)

--- prj.codeforces/33b.cpp
+++ prj.codeforces/33b.cpp
@@ -15,6 +15,7 @@ void InitializeGraph(std::vector<std::vector<int>>& graph) {
 
 int FindShortestPathBeetwenLetters(int& letter_1, const int& letter_2,
   std::vector<std::vector<int>>& graph) {
+  //std::cout << "  " << letter_2 << '\n';
   std::vector<int> paths_1(kAlphabetLenhgt, -1);
   std::vector<int> paths_2(kAlphabetLenhgt, -1);
   std::priority_queue<std::pair<int, int>> queue_1;
@@ -29,13 +30,12 @@ int FindShortestPathBeetwenLetters(int& letter_1, const int& letter_2,
       int letter = queue_1.top().second;
       temp_letter_1 = letter;
       queue_1.pop();
-      if (paths_1[letter] != -1) {
-        continue;
-      }
-      paths_1[letter] = cost;
-      for (int i_letter = 0; i_letter < kAlphabetLenhgt; i_letter += 1) {
-        if (graph[letter][i_letter] != -1) {
-          queue_1.push({ -graph[letter][i_letter] - cost, i_letter });
+      if (paths_1[letter] == -1) {
+        paths_1[letter] = cost;
+        for (int i_letter = 0; i_letter < kAlphabetLenhgt; i_letter += 1) {
+          if (graph[letter][i_letter] != -1) {
+            queue_1.push({ -graph[letter][i_letter] - cost, i_letter });
+          }
         }
       }
     }
@@ -45,13 +45,12 @@ int FindShortestPathBeetwenLetters(int& letter_1, const int& letter_2,
       int letter = queue_2.top().second;
       temp_letter_2 = letter;
       queue_2.pop();
-      if (paths_2[letter] != -1) {
-        continue;
-      }
-      paths_2[letter] = cost;
-      for (int i_letter = 0; i_letter < kAlphabetLenhgt; i_letter += 1) {
-        if (graph[letter][i_letter] != -1) {
-          queue_2.push({ -graph[letter][i_letter] - cost, i_letter });
+      if (paths_2[letter] == -1) {
+        paths_2[letter] = cost;
+        for (int i_letter = 0; i_letter < kAlphabetLenhgt; i_letter += 1) {
+          if (graph[letter][i_letter] != -1) {
+            queue_2.push({ -graph[letter][i_letter] - cost, i_letter });
+          }
         }
       }
     }
@@ -78,6 +77,8 @@ int FindShortestPathBeetwenLetters(int& letter_1, const int& letter_2,
 }
 
 int main() {
+  std::ios::sync_with_stdio(0);
+  std::cin.tie(0);
   std::string word_1 = "";
   std::string word_2 = "";
   std::cin >> word_1 >> word_2;
@@ -97,6 +98,10 @@ int main() {
       }
     }
     swaps[letter_a - 'a'][letter_b - 'a'] = cost;
+  }
+  if (word_1.size() != word_2.size()) {
+    std::cout << "-1\n";
+    return 0;
   }
   /*
   std::cout << "* ";
@@ -127,7 +132,9 @@ int main() {
   for (int i_letter = 0; i_letter < word_1.size(); i_letter += 1) {
     int letter_1 = (word_1[i_letter] - 'a');
     int letter_2 = (word_2[i_letter] - 'a');
+    //std::cout << word_2[i_letter] << '\n';
     //std::cout << static_cast<char>(letter_1 + 'a') << " - ";
+    //std::cout << " " << letter_2 << '\n';
     int current_cost = FindShortestPathBeetwenLetters(letter_1, letter_2, swaps);
     if (current_cost == -1) {
       impossible = true;
@@ -143,4 +150,5 @@ int main() {
   else {
     std::cout << overall_cost << '\n' << word_1 << '\n';
   }
+  return 0;
 }
