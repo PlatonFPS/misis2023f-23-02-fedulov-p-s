7feb78dbc1ee9b49659c36a542f5ca7f719be16f
PlatonFPS <PlatonFPS@yandex.ru>
2023-10-09 11:09:21 +0300

    Upgraded 33b(WIP)
    started to implement simpler algorithm

--- prj.codeforces/33b.cpp
+++ prj.codeforces/33b.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <map>
 
 const int kAlphabetLenhgt = 26;
 
@@ -13,66 +12,19 @@ void InitializeGraph(std::vector<std::vector<int>>& graph) {
   }
 }
 
-int FindShortestPathBeetwenLetters(int& letter_1, const int& letter_2,
-                                   std::vector<std::vector<int>>& graph) {
-  std::vector<int> paths_1(kAlphabetLenhgt, -1);
-  std::vector<int> paths_2(kAlphabetLenhgt, -1);
-  std::priority_queue<std::pair<int, int>> queue_1;
-  queue_1.push({ 0, letter_1 });
-  std::priority_queue<std::pair<int, int>> queue_2;
-  queue_2.push({ 0, letter_2 });
-  int min_cost = -1;
-  while (!queue_1.empty() || !queue_2.empty()) {
-    int temp_letter_1 = -1;
-    if (!queue_1.empty()) {
-      int cost = -queue_1.top().first;
-      int letter = queue_1.top().second;
-      temp_letter_1 = letter;
-      queue_1.pop();
-      if (paths_1[letter] == -1) {
-        paths_1[letter] = cost;
-        for (int i_letter = 0; i_letter < kAlphabetLenhgt; i_letter += 1) {
-          if (graph[letter][i_letter] != -1) {
-            queue_1.push({ -graph[letter][i_letter] - cost, i_letter });
-          }
-        }
-      }
-    }
-    int temp_letter_2 = -1;
-    if (!queue_2.empty()) {
-      int cost = -queue_2.top().first;
-      int letter = queue_2.top().second;
-      temp_letter_2 = letter;
-      queue_2.pop();
-      if (paths_2[letter] == -1) {
-        paths_2[letter] = cost;
-        for (int i_letter = 0; i_letter < kAlphabetLenhgt; i_letter += 1) {
-          if (graph[letter][i_letter] != -1) {
-            queue_2.push({ -graph[letter][i_letter] - cost, i_letter });
-          }
-        }
-      }
-    }
-    if (temp_letter_1 != -1) {
-      if (paths_2[temp_letter_1] != -1) {
-        int cost = paths_1[temp_letter_1] + paths_2[temp_letter_1];
-        if (cost < min_cost || min_cost == -1) {
-          min_cost = cost;
-          letter_1 = temp_letter_1;
-        }
-      }
-    }
-    if (temp_letter_2 != -1) {
-      if (paths_1[temp_letter_2] != -1) {
-        int cost = paths_2[temp_letter_2] + paths_1[temp_letter_2];
-        if (cost < min_cost || min_cost == -1) {
-          min_cost = cost;
-          letter_1 = temp_letter_2;
-        }
-      }
-    }
+void FindShortestPath(std::vector<int>& paths,
+  std::vector<std::vector<int>>& graph,
+  const int& start_letter) {
+  std::vector<bool> busy(kAlphabetLenhgt);
+  std::priority_queue<std::pair<int, int>> queue;
+  queue.push({ 0, start_letter });
+  while (!queue.empty()) {
+    int cost = -queue.top().first;
+    int letter = queue.top().second;
+    queue.pop();
+
   }
-  return min_cost;
+  return;
 }
 
 int main() {
@@ -107,13 +59,8 @@ int main() {
   for (int i_letter = 0; i_letter < word_1.size(); i_letter += 1) {
     int letter_1 = (word_1[i_letter] - 'a');
     int letter_2 = (word_2[i_letter] - 'a');
-    int current_cost = FindShortestPathBeetwenLetters(letter_1, letter_2, swaps);
-    if (current_cost == -1) {
-      impossible = true;
-      break;
-    }
-    overall_cost += current_cost;
-    word_1[i_letter] = static_cast<char>(letter_1 + 'a');
+    std::vector<int> paths_1(kAlphabetLenhgt, -1);
+    FindShortestPath(paths_1, swaps, letter_1);
   }
   if (impossible) {
     std::cout << "-1\n";
