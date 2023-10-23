7c8474ff5fd22b1acdf0ea348d8b9e3c8185c864
PlatonFPS <PlatonFPS@yandex.ru>
2023-10-23 19:38:47 +0300

    Updated 0004a (code style)

--- prj.codeforces/0004a.cpp
+++ prj.codeforces/0004a.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 
-int main()
-{
-    int weight;
-    std::cin >> weight;
-    if (weight > 2 && weight % 2 == 0)
-    {
-        std::cout << "YES\n";
-    }
-    else
-    {
-        std::cout << "NO\n";
-    }
+int main() {
+  int weight;
+  std::cin >> weight;
+  if (weight > 2 && weight % 2 == 0) {
+    std::cout << "YES\n";
+  }
+  else{
+    std::cout << "NO\n";
+  }
 }
