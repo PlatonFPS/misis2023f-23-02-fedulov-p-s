0fcb2bbee639862db5dd6762d1f7197b6f62a323
PlatonFPS <PlatonFPS@yandex.ru>
2023-11-26 17:52:45 +0300

    Added test_rational for testing class Rational

--- prj.lab/rational/rational.hpp
+++ prj.lab/rational/rational.hpp
@@ -6,13 +6,13 @@ public:
   explicit Rational(const int64_t num);
   Rational(const int64_t num, const int64_t den);
 
-  int64_t GetNum() { return num };
-  int64_t GetDen() { return den };
+  int64_t GetNum() { return num; }
+  int64_t GetDen() { return den; }
 
-  bool operator==(const Rational& rhs) { return num == rhs.num && den == rhs.den }
-  bool operator==(const int64_t rhs) { return operator==(Rational(rhs)) }
-  bool operator!=(const Rational& rhs) { return !(operator==(rhs)) }
-  bool operator!=(const int64_t rhs) { return operator!=(Rational(rhs)) }
+  bool operator==(const Rational& rhs) { return num == rhs.num && den == rhs.den; }
+  bool operator==(const int64_t rhs) { return operator==(Rational(rhs)); }
+  bool operator!=(const Rational& rhs) { return !(operator==(rhs)); }
+  bool operator!=(const int64_t rhs) { return operator!=(Rational(rhs)); }
 
 private:
   int64_t num = 0;
---
+++ prj.test/test_rational/test_rational.cpp
@@ -0,0 +1,6 @@
+#include <rational/rational.hpp>
+#include <iostream>
+
+int main() {
+  return 0;
+}
