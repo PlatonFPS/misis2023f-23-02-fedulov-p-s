1dbc62c775681e87160bc4e2c733a0793252cf4d
PlatonFPS <PlatonFPS@yandex.ru>
2023-12-04 00:39:59 +0300

    upd test_dynarr.cpp

--- prj.test/test_dynarr.cpp
+++ prj.test/test_dynarr.cpp
@@ -4,11 +4,28 @@
 
 #include <iostream>
 
-TEST_CASE("DynArr constructor") {
+TEST_CASE("DynArr test") {
   DynArr arr;
+
   CHECK(arr.Size() == 0);
   CHECK_THROWS(arr[0]);
   arr.Resize(5);
   CHECK(arr.Size() == 5);
   CHECK_THROWS(arr.Resize(-1));
+
+  CHECK_THROWS(arr[6]);
+
+  arr[0] = 4;
+  CHECK(arr[0] == 4);
+
+  arr[1] = 6;
+  CHECK(arr[1] == 6);
+  CHECK(arr[0] == 4);
+  CHECK(arr[0] != arr[1]);
+  
+  arr.Resize(8);
+  CHECK(arr[7] == 0);
+  
+  arr.Resize(2);
+  CHECK(arr[1] == 6);
 }
